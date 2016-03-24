92de2c916ea14bdc1ca5deae40b840980015f119
Pritchy96 <tompritchard1996@gmail.com>
2016-03-24 20:18:34 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/tom/vrvisualisertest/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.tom.vrvisualisertest;
+import android.support.v7.app.AppCompatActivity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.audiofx.Visualizer;
+import android.os.Bundle;
+
+import com.example.tom.vrvisualisertest.R;
+
+/*
+ * Copy test.mp3 to /res/raw/ folder
+ *
+ * needed in AndroidManifest.xml
+ * android:minSdkVersion="9"
+ * uses-permission of "android.permission.RECORD_AUDIO"
+ *
+ * reference: Android demo example -
+ * ApiDemos > Media > AudioTx
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    VisualizerView mVisualizerView;
+
+    private MediaPlayer mMediaPlayer;
+    private Visualizer mVisualizer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mVisualizerView = (VisualizerView) findViewById(R.id.myvisualizerview);
+
+        initAudio();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isFinishing() && mMediaPlayer != null) {
+            mVisualizer.release();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    private void initAudio() {
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        mMediaPlayer = MediaPlayer.create(this, R.raw.test);
+
+        setupVisualizerFxAndUI();
+        // Make sure the visualizer is enabled only when you actually want to
+        // receive data, and
+        // when it makes sense to receive data.
+        mVisualizer.setEnabled(true);
+        // When the stream ends, we don't need to collect any more data. We
+        // don't do this in
+        // setupVisualizerFxAndUI because we likely want to have more,
+        // non-Visualizer related code
+        // in this callback.
+        mMediaPlayer
+                .setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        mVisualizer.setEnabled(false);
+                    }
+                });
+        mMediaPlayer.start();
+
+    }
+
+    private void setupVisualizerFxAndUI() {
+
+        // Create the Visualizer object and attach it to our media player.
+        mVisualizer = new Visualizer(mMediaPlayer.getAudioSessionId());
+        mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+        mVisualizer.setDataCaptureListener(
+                new Visualizer.OnDataCaptureListener() {
+                    public void onWaveFormDataCapture(Visualizer visualizer,
+                                                      byte[] bytes, int samplingRate) {
+                        mVisualizerView.updateVisualizer(bytes);
+                    }
+
+                    public void onFftDataCapture(Visualizer visualizer,
+                                                 byte[] bytes, int samplingRate) {
+                    }
+                }, Visualizer.getMaxCaptureRate() / 2, true, false);
+    }
+
+}
