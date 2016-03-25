06d7804da7680cb68287917f2f292b8aef4e24bc
Pritchy96 <tompritchard1996@gmail.com>
2016-03-25 15:19:22 +0000

    Began to add mic code, added resume code.

--- app/src/main/java/com/example/tom/vrvisualisertest/MainActivity.java
+++ app/src/main/java/com/example/tom/vrvisualisertest/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.tom.vrvisualisertest;
 
+import android.annotation.TargetApi;
+import android.media.AudioFormat;
 import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.media.audiofx.Visualizer;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
   private VisualizerView visualiserView;
   private Visualizer visualiser;
+  private AudioRecord audioRecorder;
+  private static final int RECORDING_SAMPLE_RATE = 44100;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
     setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
     //Setup the visualiser, choosing whether to use audio from another app or microphone.
-    setupVisualiser(false, false); //True: Use microphone, use Waveform data.
+    setupVisualiser(false
+        , false, true); //True: Use microphone, collect waveform data, convert to fft.
   }
 
   @Override
@@ -31,16 +40,40 @@ public class MainActivity extends AppCompatActivity {
     }
   }
 
+    @Override
+    protected void onResume() {
+      super.onResume();
+    }
+
   @Override
   protected void onDestroy() {
     super.onDestroy();
     visualiserView.setEnabled(false);
   }
 
-  private void setupVisualiser(Boolean useMicrophone, Boolean waveForm) {
+  private void initMicrophone() {
+    int bufferSize = AudioRecord.getMinBufferSize(
+        RECORDING_SAMPLE_RATE,
+        AudioFormat.CHANNEL_IN_MONO,
+        AudioFormat.ENCODING_PCM_16BIT
+    );
+
+    audioRecorder = new AudioRecord(
+        MediaRecorder.AudioSource.MIC,
+        RECORDING_SAMPLE_RATE,
+        AudioFormat.CHANNEL_IN_MONO,
+        AudioFormat.ENCODING_PCM_16BIT,
+        bufferSize
+    );
+    //audioRecorder.startRecording();
+  }
+
+
+  private void setupVisualiser(Boolean useMicrophone, Boolean getWaveform, Boolean getFft) {
     // Create the Visualizer object and attach it to our input.
     if (useMicrophone) {
-      //visualiser = new Visualizer(CODE TO SETUP MICROPHONE INPUT HERE);
+      initMicrophone();
+      visualiser = new Visualizer(audioRecorder.getAudioSessionId());
     } else {
       visualiser = new Visualizer(0);
     }
@@ -57,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
           public void onFftDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {
             visualiserView.updateVisualizerWithFft(bytes);
           }
-        }, Visualizer.getMaxCaptureRate() / 2, waveForm, !waveForm);  //Choose to get waveform or fft output.
+        }, Visualizer.getMaxCaptureRate() / 2, getWaveform, getFft);  //Choose to get waveform, fft output.
     visualiser.setEnabled(true); //Enabled only when needed, after setCaptureSize.
   }
 }
