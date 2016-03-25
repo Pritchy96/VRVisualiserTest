c5fd319805eea88d3f4331473e3e95d10c46d65c
Pritchy96 <tompritchard1996@gmail.com>
2016-03-25 02:07:15 +0000

    Further clean up, switched to point rendering

--- app/src/main/java/com/example/tom/vrvisualisertest/MainActivity.java
+++ app/src/main/java/com/example/tom/vrvisualisertest/MainActivity.java
@@ -1,64 +1,63 @@
 package com.example.tom.vrvisualisertest;
-import android.support.v7.app.AppCompatActivity;
+
 import android.media.AudioManager;
 import android.media.audiofx.Visualizer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private VisualizerView visualiserView;
-    private Visualizer visualiser;
+  private VisualizerView visualiserView;
+  private Visualizer visualiser;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        visualiserView = (VisualizerView) findViewById(R.id.myvisualizerview);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    visualiserView = (VisualizerView) findViewById(R.id.myvisualizerview);
 
-        //Set audio adjustment to Media instead of phone ringing volume.
-        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+    //Set audio adjustment to Media instead of phone ringing volume.
+    setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
-        //Setup the visualiser, choosing whether to use audio from another app or microphone.
-        setupVisualiser(false); //True = use microphone.
+    //Setup the visualiser, choosing whether to use audio from another app or microphone.
+    setupVisualiser(false); //True = use microphone.
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    if (isFinishing()) {
+      visualiser.release();
+    }
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    visualiserView.setEnabled(false);
+  }
+
+  private void setupVisualiser(Boolean useMicrophone) {
+    // Create the Visualizer object and attach it to our input.
+    if (useMicrophone) {
+      //visualiser = new Visualizer(CODE TO SETUP MICROPHONE INPUT HERE);
+    } else {
+      visualiser = new Visualizer(0);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (isFinishing()) {
-            visualiser.release();
-        }
-    }
+    visualiser.setCaptureSize(Visualizer.getCaptureSizeRange()[1]); //Set capture size to max.
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        visualiserView.setEnabled(false);
-    }
+    //Setup listener to fill bytes with waveform data every sampleRate time.
+    visualiser.setDataCaptureListener(
+        new Visualizer.OnDataCaptureListener() {
+          public void onWaveFormDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {
+            visualiserView.updateVisualizer(bytes);
+          }
 
-    private void setupVisualiser(Boolean useMicrophone) {
-        // Create the Visualizer object and attach it to our input.
-        if (useMicrophone) {
-            //visualiser = new Visualizer(CODE TO SETUP MICROPHONE INPUT HERE);
-        } else {
-            visualiser = new Visualizer(0);
-        }
+          public void onFftDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {
+          }
+        }, Visualizer.getMaxCaptureRate() / 2, true, false);
 
-        visualiser.setCaptureSize(Visualizer.getCaptureSizeRange()[1]); //Set capture size to max.
-
-        //Setup listener to fill bytes with waveform data every sampleRate time.
-        visualiser.setDataCaptureListener(
-                new Visualizer.OnDataCaptureListener() {
-                    public void onWaveFormDataCapture(Visualizer visualizer,
-                                                      byte[] bytes, int samplingRate) {
-                        visualiserView.updateVisualizer(bytes);
-                    }
-
-                    public void onFftDataCapture(Visualizer visualizer,
-                                                 byte[] bytes, int samplingRate) {
-                    }
-                }, Visualizer.getMaxCaptureRate() / 2, true, false);
-
-        visualiser.setEnabled(true); //Enabled only when needed, after setCaptureSize.
-    }
+    visualiser.setEnabled(true); //Enabled only when needed, after setCaptureSize.
+  }
 }
--- app/src/main/java/com/example/tom/vrvisualisertest/VisualizerView.java
+++ app/src/main/java/com/example/tom/vrvisualisertest/VisualizerView.java
@@ -3,68 +3,72 @@ package com.example.tom.vrvisualisertest;
 /**
  * Created by Tom on 24/03/2016.
  */
-        import android.content.Context;
-        import android.graphics.Canvas;
-        import android.graphics.Color;
-        import android.graphics.Paint;
-        import android.graphics.Rect;
-        import android.util.AttributeSet;
-        import android.view.View;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
 
 public class VisualizerView extends View {
 
-    private byte[] audioBytes;
-    private float[] drawPoints;
-    private Rect screenRect = new Rect();
-    private Paint mForePaint = new Paint();
+  private byte[] audioBytes;
+  private float[] drawPoints;
+  private Rect screenRect = new Rect();
+  private Paint paint = new Paint();
 
-    public VisualizerView(Context context) {
-        super(context);
-        init();
+
+  public VisualizerView(Context context) {
+    super(context);
+    init();
+  }
+
+  public VisualizerView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    init();
+  }
+
+  public VisualizerView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  private void init() {
+    audioBytes = null;
+    paint.setStrokeWidth(1.5f);
+    paint.setAntiAlias(true);
+    paint.setColor(Color.rgb(0, 128, 0));
+  }
+
+  public void updateVisualizer(byte[] bytes) {
+    audioBytes = bytes;
+    invalidate();
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+    if (audioBytes == null) {
+      return;
+    }
+    if (drawPoints == null || drawPoints.length < audioBytes.length * 4) {
+      drawPoints = new float[audioBytes.length * 2];
     }
 
-    public VisualizerView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init();
-    }
+    screenRect.set(0, 0, getWidth(), getHeight());  //Redefine screen view.
 
-    public VisualizerView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
-    }
+    //Create array of Points to draw.
+    for (int i = 0; i < audioBytes.length - 1; i++) {
+      //Point 1 x.
+      drawPoints[i * 2] = screenRect.width() * i / (audioBytes.length - 1);
+      //Point 1 y. Add screenRect.Height/2 to make it centered on screen, + 128 to get it to 0 - 256.
+      drawPoints[i * 2 + 1] = (screenRect.height() / 2) + ((byte) (audioBytes[i] + 128)) * (screenRect.height() / 2) / 128;
 
-    private void init() {
-        audioBytes = null;
-        mForePaint.setStrokeWidth(1.5f);
-        mForePaint.setAntiAlias(true);
-        mForePaint.setColor(Color.rgb(0, 128, 0 ));
+      //System.out.println(audioBytes[i]);  //DEBUG.
     }
-
-    public void updateVisualizer(byte[] bytes) {
-        audioBytes = bytes;
-        invalidate();
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        if (audioBytes == null) {
-            return;
-        }
-        if (drawPoints == null || drawPoints.length < audioBytes.length * 4) {
-            drawPoints = new float[audioBytes.length * 4];
-        }
-        screenRect.set(0, 0, getWidth(), getHeight());
-        for (int i = 0; i < audioBytes.length - 1; i++) {
-            drawPoints[i * 4] = screenRect.width() * i / (audioBytes.length - 1);
-            drawPoints[i * 4 + 1] = screenRect.height() / 2
-                    + ((byte) (audioBytes[i] + 128)) * (screenRect.height() / 2) / 128;
-            drawPoints[i * 4 + 2] = screenRect.width() * (i + 1) / (audioBytes.length - 1);
-            drawPoints[i * 4 + 3] = screenRect.height() / 2
-                    + ((byte) (audioBytes[i + 1] + 128)) * (screenRect.height() / 2)
-                    / 128;
-        }
-        canvas.drawLines(drawPoints, mForePaint);
-    }
+    canvas.drawPoints(drawPoints, paint);
+}
 
 }
